73b2d51b04a463828fed879972f9357a30f5472c
ricardopresto <ricardopresto5@gmail.com>
2019-10-25 20:36:45 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const MongoClient = require("mongodb").MongoClient;
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+const port = 8000;
+
+const dbName = "test";
+const url =
+  "mongodb+srv://ricardopresto:ricardo123@cluster0-yuyny.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useUnifiedTopology: true });
+client.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.set("view engine", "ejs");
+
+/*app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/views/index.ejs");
+});
+*/
+
+app.get("/posts", async (req, res) => {
+  const posts = await getPosts();
+  res.render("view.ejs", { words: posts });
+});
+
+app.post("/posts", (req, res) => {
+  sendPost(req.body);
+  res.redirect("/posts");
+});
+
+async function sendPost(post) {
+  try {
+    const db = client.db(dbName);
+    await db.collection("posts").insertOne(post);
+  } catch (err) {
+    console.log(err.stack);
+  }
+}
+
+async function getPosts() {
+  let docs;
+  try {
+    const db = client.db(dbName);
+    docs = await db
+      .collection("posts")
+      .find({})
+      .toArray();
+  } catch (err) {
+    console.log(err.stack);
+  }
+  return docs;
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
